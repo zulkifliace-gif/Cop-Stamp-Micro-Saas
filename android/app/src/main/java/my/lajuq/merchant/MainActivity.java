ed66213f23045c384e5a7e3b63583ee4679a2606
TradingMaster <zulkifliace@gmail.com>
2026-09-04 18:07:25 +0800

    fix(mobile): prevent opening external browser by whitelisting www.lajuq.my and adding hardware back support

--- android/app/src/main/java/my/lajuq/merchant/MainActivity.java
+++ android/app/src/main/java/my/lajuq/merchant/MainActivity.java
@@ -1,5 +1,20 @@
 package my.lajuq.merchant;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (this.bridge != null && this.bridge.getWebView() != null && this.bridge.getWebView().canGoBack()) {
+            this.bridge.getWebView().goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,9 +5,18 @@ const config: CapacitorConfig = {
   appName: 'LajuS Merchant',
   webDir: 'public',
   server: {
-    url: 'https://lajuq.my/dashboard',
+    url: 'https://www.lajuq.my/dashboard',
     cleartext: false,
     androidScheme: 'https',
+    allowNavigation: [
+      'www.lajuq.my',
+      'lajuq.my',
+      '*.lajuq.my',
+      '*.supabase.co',
+      'accounts.google.com',
+      '*.google.com',
+      'accounts.youtube.com',
+    ],
   },
   android: {
     allowMixedContent: true,
